7e44e884562a9ca583e7de390b4fae37b403625d
Arthur Koziel <arthur@arthurkoziel.com>
2018-06-17 01:15:42 +0200

    apistar: return 201 with location header when creating article

--- python/apistar/app.py
+++ python/apistar/app.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from apistar import ASyncApp, Route, exceptions, http
+from apistar import App, ASyncApp, Route, exceptions, http
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from common import MemoryRepo  # noqa
@@ -22,13 +22,17 @@ async def get_article(article_id: int) -> dict:
         raise exceptions.NotFound()
     return article
 
-async def create_article(data: http.RequestData, password: http.Header):
+async def create_article(app: App, data: http.RequestData,
+                         password: http.Header) -> http.JSONResponse:
     # if not is_auth(password):
     #     raise exceptions.BadRequest()
     if not data['title']:
         raise exceptions.BadRequest()
 
-    repo.add_article({'title': data['title']})
+    article_id = repo.add_article({'title': data['title']})
+    url = app.reverse_url('get_article', article_id=article_id)
+    headers = {'Location': url}
+    return http.JSONResponse({}, status_code=201, headers=headers)
 
 async def update_article(article_id: int, data: http.RequestData,
                          password: http.Header):
@@ -63,4 +67,4 @@ routes = [
 app = ASyncApp(routes=routes)
 
 if __name__ == '__main__':
-    app.serve('127.0.0.1', 8000, debug=False)
+    app.serve('127.0.0.1', 8000, debug=True)
